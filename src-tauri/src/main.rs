0505ceef7ff5aaca36d4afb6c7f6a240c23e5e05
tronganh0104 <tronganhsl93@gmail.com>
2026-05-31 13:27:19 +0700

    feat: add conditional compilation for Windows subsystem in main function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,6 @@
-﻿fn main() {
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running MushroomLearning Desktop");
